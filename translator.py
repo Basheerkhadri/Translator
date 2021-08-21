476521cb52a1ba59e733966346b1210c2cabb83b
Basheerkhadri <basheerkhadri@gmail.com>
2021-08-22 01:33:54 +1000

    adding UTF-8

--- translator.py
+++ translator.py
@@ -8,7 +8,7 @@ try:
     with open('./note.txt', mode='r') as my_file:
      text= my_file.read()
      translation = translator.translate(text)
-     with open('./note-ja.txt', mode='w') as my_file2:
+     with open('./note-ja.txt', mode='w', encoding="utf-8") as my_file2:
              my_file2.write(translation)
 except FileNotFoundError as err:
         print('check your file path idiot!')
